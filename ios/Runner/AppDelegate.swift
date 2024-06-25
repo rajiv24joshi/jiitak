122502b53899165991bf62228214210c0ca99e92
Rajiv <rajiv@kodiary.com>
2024-06-25 10:16:31 +0545

    added ios configs and android configs

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? 
   ) -> Bool {
-    [GMSServices provideAPIKey:@"YOUR KEY HERE"] // Put jittaks key
+    // [GMSServices provideAPIKey:@"YOUR KEY HERE"] // Put jittaks key
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
